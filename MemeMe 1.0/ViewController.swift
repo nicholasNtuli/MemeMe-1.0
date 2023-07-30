5bb3ad8246d20b7e77c0f830db659cd31cf7c0bc
Sihle Ntuli <sihle.ntuli@glucode.com>
2023-07-30 13:05:17 +0200

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Sihle Ntuli on 2023/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
